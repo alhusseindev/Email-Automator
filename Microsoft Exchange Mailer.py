8b88bdcf6ae070920a46868b947486882ef36fe6
alhusseindev <55993187+alhusseindev@users.noreply.github.com>
2021-07-14 15:45:13 +0200

    Update Microsoft Exchange Mailer.py

--- Microsoft Exchange Mailer.py
+++ Microsoft Exchange Mailer.py
@@ -34,41 +34,68 @@ messages_retrieved_from_junkfolder = junk_folder.get_messages(limit= 5, download
 
 
 
+
 #Taking care of messages
-our_messages = []
-with open("Todays Leads.txt", "w+") as leads_file:
+blacklisted_countries = ["abc", "def", "ghi"] #Countries With High Number of Spammers!
+blacklisted_keywords = ["abcSpam", "defSpam", "ghiSpam"] #keywords that spammers use
+skip_message = False
+
+with open("Leads.txt", "a+", encoding="UTF-8") as leads_file:
     for message in messages_retrieved_from_junkfolder:
-        if(message.subject == "Interested Lead" and message.created.day == todays_day):
+        if(message.subject == "Some Subject" and message.created.day == todays_day):
+            print("Found Email That I am Interested In!")
             message_body = message.get_body_text()
-            print(message_body)
-            for line in message_body:
-                leads_file.writeline("Leads")
+	    message_body.lower()
+            for country in blacklisted_countries:
+                if(country in message_body):
+                    print("BlackListed Country! Skip this inquiry. ", "Country: " + country)
+                    skip_message = True
+                    #break out of the country loop
+                    break
+               
+            for keyword in blacklisted_keywords:
+                if(keyword in message_body):
+                    print("Blacklisted Keyword! Skip this inquiry.", "Keyword: " + keyword)
+                    skip_message = True
+                    #break out of the keyword loop
+                    break
+
+            if(not skip_message):
+                leads_file.write("--------------------------------------------------\nLead:\n--------------------------------------------------\n")
                 leads_file.write(message_body)
-            print("Message Date: ", message.created.day)
-            #print(message_body)
+
             
 
+        else:
+            print("Not Interested in Email!")
+            
+                
 
 
-"""
-for i in our_messages:
-   print(i.body)
-"""
+#checking the file
+with open("Leads.txt", 'r') as file_being_read:
+    for line in file_being_read:
+        if(len(line) == 0):
+            print("Leads File Is Empty, Will Not Send Email!")
+    
+    """ Sending Leads """
+    try:
+        my_message = mailbox.new_message()
+        my_message.to.add(["email@email.com"])
+        my_message.subject = "FWD: Lead(s)"
+        my_message.body = "Lead(s)\n"
+        my_message.attachments.add("Leads.txt")
+        my_message.send()
+        print("Message Sent Successfully")
+    except Exception:
+        logging.basicConfig(filename= "email_error.log", filemode= "w", level= logging.DEBUG) 
+        print("Log File Saved Successfully")
 
 
+            
+#Deleting the file after it's being sent
+os.remove("Leads.txt")
+print("Leads File Deleted Successfully")
 
 
-""" Sending Leads """
-try:
-    my_message = mailbox.new_message()
-    my_message.to.add(["my_email"])
-    my_message.subject = "FWD: Lead"
-    my_message.body = "Lead\n"
-    my_message.attachments.add("Todays Leads.txt")
-    my_message.send()
-    print("Message Sent Successfully")
-except Exception:
-    with open("log.txt", 'w+') as logfile:
-        logfile.write(traceback.print_exc())
-    print("Log File Saved Successfully")
 
